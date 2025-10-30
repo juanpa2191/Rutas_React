477c1980cb6e0c235fef994b4c6d0d85b9174907
Juan Pablo Ospina <juanpa2191@hotmail.com>
2025-10-30 07:40:46 -0500

    creacion de paginas principales

---
+++ ruta/src/pages/Contact.tsx
@@ -0,0 +1,9 @@
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>This is the contact page of our application.</p>
+    </div>
+  );
+}
+export default Contact;
---
+++ ruta/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+function Home(){
+    return(
+        <div>
+            <h1>Bienvenido clase UNAC</h1>
+            <p>Esta pagina es para que los compañeros puedan ingresar e instruirse con el proyecto de react</p>
+            <p>Espero que les guste y puedan aprender mucho</p>
+        </div>
+    );
+}
+
+export default Home;
